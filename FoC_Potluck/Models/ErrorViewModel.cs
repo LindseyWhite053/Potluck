babe8e2092005c1af0b57bd1fe213799bff88c74
Lindsey White <LindseyWhite@RocketMortgage.com>
2022-09-23 13:22:47 -0400

    initial commit

---
+++ FoC_Potluck/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FoC_Potluck.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
